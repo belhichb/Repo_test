9b1be428ff2514fd87e08a02c4c7a26d0cb4687b
belhichb <belhichb@gmail.com>
2022-05-11 21:10:59 +0100

    define student class

--- classes_test.py
+++ classes_test.py
@@ -3,3 +3,11 @@
 #my second commit
 #test PC
 #this line is added in github
+
+class Student():
+    def __init__(self,name,cin,bth_day):
+        self.name=name
+        self.cin=cin
+        self.bth_day=bth_day
+
+Hicham=Student("hicham",123456,1985)
