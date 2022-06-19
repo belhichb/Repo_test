8a1bad3014fafc24810437346706a46e67421e24
belhichb <belhichb@gmail.com>
2022-06-19 18:06:38 +0100

    polymorphism

--- SubClass_test.py
+++ SubClass_test.py
@@ -1,30 +1,40 @@
-#this file is used to test  sub classes
-# Define a Clothing superclass with wear and sell instance methods.
-# The wear method should return the string “I'm wearing this fashionable piece of clothing!”
-# The sell method should return the string “Buy my piece of clothing!”
+# Declare a Musician class that accepts a name argument in its initialization.
+# The initialization should assign a name argument to a name attribute. 
+# In addition, each Musician object should have an "albums" attribute 
+# that begins as an empty list.
+# Define a release_album instance method on a Musician that accepts a title (string). 
+# It should append the string to the end of the list held by the
+# albums attribute (see examples below).
+#
+# Declare a Drummer subclass than inherits from the Musician superclass. 
+# In addition to a name, a drummer should also have a stamina attribute 
+# represented by an integer. 
+# The subclass should invoke the Musician superclass’s initialization procedure
+# and pass it the drummer’s name.
+# It should also set the stamina attribute in its own initialization process.
+#
 
-# Define a Socks subclass that inherits from the Clothing superclass.
-# It should define a lose_one instance method that 
-# returns the string “Where did my other one go?”
-# It should override the wear method to 
-# return the string “Take a look at my socks! They're gorgeous!”
-# It should override the sell method to 
-# return the string “Buy my socks!”
 
+class Musician():
+    def __init__(self,name):
+        self.name=name
+        self.albums=[]
+    def release_album(self,title):
+        self.albums.append(title)
 
-
-class Clothing():
-    def __init__(self):
-        pass
-    def wear(self):
-        return "I'm wearing this fashionable piece of clothing!"
-    def sell(self):
-        return "Buy my piece of clothing!"
+class Drummer(Musician):
+    def __init__(self,name,stamina):
+        super().__init__(name)
+        self.stamina=stamina
         
-class Socks(Clothing):
-    def lose_one(self):
-        return 'Where did my other one go?'
-    def wear(self):
-        return "Take a look at my socks! They're gorgeous!"
-    def sell(self):
-        return "Buy my socks!"
+# EXAMPLE:
+lars = Drummer(name = "Lars", stamina = 2)
+print(lars.name)   # "Lars"
+print(lars.stamina) # 2
+print(lars.albums) # []
+
+lars.release_album("Ride the Lightning")
+print(lars.albums) # ["Ride the Lightning"]
+#
+lars.release_album("Master of Puppets")
+print(lars.albums)  # ["Ride the Lightning", 'Master of Puppets']
